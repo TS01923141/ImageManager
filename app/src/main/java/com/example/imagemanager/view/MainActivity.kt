b07674a354f674ab8bcf06da242d450b671e18aa
Tsai <yichen.tsai@wvt.com.tw>
2022-02-18 16:13:27 +0800

    bottomBar連動Fragment完成

---
+++ app/src/main/java/com/example/imagemanager/view/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.imagemanager.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.imagemanager.R
+import com.example.imagemanager.databinding.ActivityMainBinding
+
+/*
+    想做的功能
+    抓所有圖片做成album
+    透過FilePicker選圖
+    透過URL複製圖片
+    scan新圖
+    從internal點圖片分享
+    下載圖片
+
+    --
+    一個View配BottomBar
+    bottomBar分所有圖片的album跟internal圖片album（兩個Fragment）
+    internal右上角
+     1.選圖可以透過FilePicker加圖進來
+     2.dialog輸入網址下載圖片加圖進來
+
+    所有圖片
+    透過ContentProvider取得所有圖片
+
+    RecyclerView用GridManager一排三張圖
+    點擊可以分享
+    --
+    圖片嘗試用coil不用Glide
+    分區儲存檢測
+ */
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //set bottomBar and navController
+        val navController = findNavController(R.id.navHostFrag_main)
+        binding.navViewMain.setupWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/com/example/imagemanager/view/album/AlbumFragment.kt
@@ -0,0 +1,23 @@
+package com.example.imagemanager.view.album
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.imagemanager.databinding.FragmentAlbumBinding
+
+class AlbumFragment: Fragment() {
+    private var _binding : FragmentAlbumBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = FragmentAlbumBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
